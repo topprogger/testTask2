4ddd7e2a28e33cd91bcec55ab8f8a00e799de6b9
Паша Шилик <derzkiyderzkiy@outlook.com>
2019-10-10 18:29:50 +0300

    init

--- Sorter/Program.cs
+++ Sorter/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sorter
 {
@@ -10,6 +8,27 @@ namespace Sorter
     {
         static void Main(string[] args)
         {
+            List<int> mas = new List<int> ();
+            Random random=new Random();
+  
+            for (int i = 0; i< random.Next(150); i++)
+            {
+                mas.Add(random.Next(500));
+            }
+            for (int ind=0; ind<mas.Count;ind++)
+            {
+                if (mas[ind] < 14)
+                {
+                    mas.Remove(ind);
+                }
+            }
+            mas = mas.OrderByDescending(x => x)                 
+                  .ToList();
+            for(int a=0;a< mas.Count;a++)
+            {
+                Console.WriteLine(mas[a]);
+            }
+            Console.ReadKey();
         }
     }
 }
